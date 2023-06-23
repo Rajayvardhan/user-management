b828e8b3f11f47bed40d140dfa17a609cb8de025
Rajayvardhan <rvsrathore2002@gmail.com>
2023-06-23 12:24:59 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,49 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Susan',
+    lastName: 'Jordon',
+    email: 'susan@example.com',
+    phone: '950676574',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Adrienne',
+    lastName: 'Doak',
+    email: 'adrienne@example.com',
+    phone: '950676574',
+
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Rolf',
+    lastName: 'Hegdal',
+    email: 'rolf@example.com',
+    phone: '950676574',
+
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Kent',
+    lastName: 'Rosner',
+    email: 'kent@example.com',
+    phone: '950676574',
+
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Arsenio',
+    lastName: 'Grant',
+    email: 'arsenio@example.com',
+    phone: '950676574',
+
+    date: '2019-06-13'
+  },
+
+];
+
+export { employeesData };
